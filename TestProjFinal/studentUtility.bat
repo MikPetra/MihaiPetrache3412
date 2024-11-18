2a9cd8572e1189042e2f55e1cf36b09a08b2c503
MikPetra <164607089+MikPetra@users.noreply.github.com>
2024-11-18 14:20:34 +0200

    Update studentUtility.bat

--- TestProjFinal/studentUtility.bat
+++ TestProjFinal/studentUtility.bat
@@ -1,4 +1,4 @@
 @echo off
 @REM PathToPythonExe PathToStudentUtilityPythonScriptLocation
-"C:/Python3.11.1/python.exe" "d:/Fac/FacUB/Anu 2/MAP/MihaiPetrache3412/TestProjFinal/studentUtility.py"
+"C:/Python3.11.1/python.exe" "C:/path/to/the/script/studentUtility.py"
 
