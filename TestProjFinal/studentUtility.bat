3bc4bece6200053e6e04482e2996f1f8d3fa33f2
MikPetra <164607089+MikPetra@users.noreply.github.com>
2024-11-24 11:27:07 +0200

    Update studentUtility.bat

--- TestProjFinal/studentUtility.bat
+++ TestProjFinal/studentUtility.bat
@@ -1,4 +1,5 @@
 @echo off
 @REM PathToPythonExe PathToStudentUtilityPythonScriptLocation
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
 "C:/Python3.11.1/python.exe" "C:/path/to/the/script/studentUtility.py"
-
+exit
